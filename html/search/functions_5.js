5c4bfaac3c3a3762249424a5862904e754682619
JohnParamol <envdovin@mail.ru>
2022-12-02 14:18:24 +0100

    update

---
+++ html/search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['test_5flut_0',['test_lut',['../c__api_8cpp.html#a392f53e0e4e722492dc43f03acee495d',1,'test_lut():&#160;c_api.cpp'],['../c__api_8h.html#a392f53e0e4e722492dc43f03acee495d',1,'test_lut():&#160;c_api.cpp']]]
+];
---
+++ html/search/typedefs_0.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['terrorcallback_0',['TErrorCallback',['../namespacexg__core.html#aab0c64228fa5acaa817782ca131b8203',1,'xg_core']]],
+  ['thandlelut1d_1',['THandleLut1D',['../namespacexg__core.html#aa1fa6a057dd233ca2a25d1abe8220a84',1,'xg_core']]],
+  ['thandlelut3d_2',['THandleLut3D',['../namespacexg__core.html#aa0c75afbac4f0f33eeb641222b7d10df',1,'xg_core']]],
+  ['thandlenode_3',['THandleNode',['../namespacexg__core.html#a6f79970a5e51f5f931426ab952ae12e8',1,'xg_core']]],
+  ['thandlenodeclassdescription_4',['THandleNodeClassDescription',['../namespacexg__core.html#af1b09fae0560ef8f7a4e8e3a3e9057e2',1,'xg_core']]],
+  ['thandlenodemanager_5',['THandleNodeManager',['../namespacexg__core.html#a03f7601d89e9d7024a70af8cd8e87415',1,'xg_core']]],
+  ['thandlenodeparam_6',['THandleNodeParam',['../namespacexg__core.html#a7ffae7e9866e3aeaadb49334711719f8',1,'xg_core']]]
+];
