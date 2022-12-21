7868e9877ab4c386ff9ef019dded26c4c5ebb9ec
JohnParamol <envdovin@mail.ru>
2022-12-21 11:06:41 +0100

    update

--- html/search/typedefs_0.js
+++ html/search/typedefs_0.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['hlut1d_0',['HLut1D',['../namespacexg__core.html#a733ea2b4a7bf7d5cef48e6eeaa35ab96',1,'xg_core']]],
-  ['hlut3d_1',['HLut3D',['../namespacexg__core.html#a6e28a71588200e95ba7102d40f594761',1,'xg_core']]],
-  ['hnodeclassinfo_2',['HNodeClassInfo',['../namespacexg__core.html#aafb541242859a41779e9f41319caf59d',1,'xg_core']]],
-  ['hnodemanager_3',['HNodeManager',['../namespacexg__core.html#add57fccbcd6d1c0af0fd767f051dc265',1,'xg_core']]]
+  ['hcontext_0',['HContext',['../namespacexg__core.html#aed4dfc63962193e33f30f3307f2aa4a1',1,'xg_core']]],
+  ['hcontextclient_1',['HContextClient',['../namespacexg__core.html#a99b9168f16741acc26f761504e366bcb',1,'xg_core']]],
+  ['hlut1d_2',['HLut1D',['../namespacexg__core.html#a733ea2b4a7bf7d5cef48e6eeaa35ab96',1,'xg_core']]],
+  ['hlut3d_3',['HLut3D',['../namespacexg__core.html#a6e28a71588200e95ba7102d40f594761',1,'xg_core']]],
+  ['hnodeclassinfo_4',['HNodeClassInfo',['../namespacexg__core.html#aafb541242859a41779e9f41319caf59d',1,'xg_core']]],
+  ['hnodemanager_5',['HNodeManager',['../namespacexg__core.html#add57fccbcd6d1c0af0fd767f051dc265',1,'xg_core']]]
 ];
