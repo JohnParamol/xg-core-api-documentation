a4eb9edb061efcddc1370836a21f596c63427e21
JohnParamol <envdovin@mail.ru>
2022-12-26 13:16:58 +0100

    update

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -5,6 +5,5 @@ var searchData=
   ['hcontextclient_2',['HContextClient',['../namespacexg__core.html#a99b9168f16741acc26f761504e366bcb',1,'xg_core']]],
   ['hlut1d_3',['HLut1D',['../namespacexg__core.html#a733ea2b4a7bf7d5cef48e6eeaa35ab96',1,'xg_core']]],
   ['hlut3d_4',['HLut3D',['../namespacexg__core.html#a6e28a71588200e95ba7102d40f594761',1,'xg_core']]],
-  ['hnodeclassinfo_5',['HNodeClassInfo',['../namespacexg__core.html#aafb541242859a41779e9f41319caf59d',1,'xg_core']]],
-  ['hnodemanager_6',['HNodeManager',['../namespacexg__core.html#add57fccbcd6d1c0af0fd767f051dc265',1,'xg_core']]]
+  ['hnodemanager_5',['HNodeManager',['../namespacexg__core.html#add57fccbcd6d1c0af0fd767f051dc265',1,'xg_core']]]
 ];
